e376c87fa4ec32ddd44b7ccb151703784d715811
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2023-09-03 05:29:14 +0530

    adding the post router and controller and adding middleware function

--- index.js
+++ index.js
@@ -7,12 +7,37 @@ const port = 8000;
 app.set('view engine', "ejs");
 app.set("views", "./views");
 
+// adding middleware
+app.use(express.urlencoded());
+
+var taskContainer = [
+    // {
+    //     task : "create task",
+    //     dueDate : "Date",
+    //     category : "Category"    
+    // }
+]
+
+
+
+
 app.get("/", (req,res)=>{
     return res.render('home',{
-        title : 'Todo-List'
-    })
+        title : 'Todo-List',
+        task_container : taskContainer
+    });
 })
 
+app.post("/create_task", (req,res)=>{
+    console.log(req.body);
+    taskContainer.push(req.body);
+    return res.redirect("/");
+})
+
+
+// app.get("/delete_task",(req,res)=>{
+
+// })
 app.listen(port, (err)=>{
     if (err){
         console.log(`Error in running the express server : ${err}`);
